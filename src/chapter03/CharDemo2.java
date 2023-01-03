4e13009f89bd72cbee139ecd0f10583884d8ad50
Vadimir <vadymir@MacBook-Air-Vadimir.local>
2023-01-03 20:03:44 +0100

    девятый пример

---
+++ src/chapter03/CharDemo2.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+// символьные переменные ведут себя как целочисленные значения
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'X';
+        System.out.println("ch1 содержит " + ch1);
+
+        ch1++; // увеличиваем на 1
+        System.out.println("ch1 теперь содержить " + ch1);
+    }
+}
+
