ea0f6564d1c5978b335e8f0108a011fb74367f46
Vadimir <vadymir@MacBook-Air-Vadimir.local>
2023-01-03 19:52:12 +0100

    седьмой пример

---
+++ src/chapter03/Area.java
@@ -0,0 +1,13 @@
+package chapter03;
+
+// Вычмсляем площадь круга
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 10.8; // радиус окружности
+        pi = 3.1416; // число пи
+        a = pi*r*r; // вычисляем площадь круга
+        System.out.println("Площадь круга равна  " + a);
+
+    }
+}
