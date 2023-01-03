c434d5f852217b7b05e8f54e9826e379813122cb
Vadimir <vadymir@MacBook-Air-Vadimir.local>
2023-01-03 17:10:38 +0100

    Initial commit

---
+++ src/chapter02/Example.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+/* Это простая программа на Java
+   Присвоить исходному файлу имя Example.java */
+public class Example {
+    // Эта программаначнается с вызова метода main
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+    }
+}
