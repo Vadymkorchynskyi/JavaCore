963d7227d7dead895f7d670b213fb6760f4567a0
Vadimir <vadymir@MacBook-Air-Vadimir.local>
2023-01-03 19:58:27 +0100

    восьмой пример

---
+++ src/chapter03/CharDemo.java
@@ -0,0 +1,13 @@
+package chapter03;
+
+// демонстрация использования символов char
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88; // код симола Х
+        ch2 = 'Y';
+        System.out.print("ch1 и ch2:  ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
