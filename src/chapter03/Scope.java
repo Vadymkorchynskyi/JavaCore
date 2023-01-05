089a7a9fd1e136ac583625ba3fdb744324a88885
Vadimir <vadymir@MacBook-Air-Vadimir.local>
2023-01-05 16:59:19 +0100

    12-й пример

---
+++ src/chapter03/Scope.java
@@ -0,0 +1,14 @@
+package chapter03;
+// демонстрация области действия
+public class Scope {
+    public static void main(String[] args) {
+        int x; // эта переменная доступна всему классу
+        x = 10;
+        if (x == 10) { //начало новой области действия
+            int y = 20; // доступна только этому блоку кода
+            System.out.println(x + " " + y);
+            x = y*2;
+        }
+        System.out.println(x + " " + y); // как видим здесь У уже недоступна
+    }
+}
